94f17a06b00ed09845ded2f4c61c2c6b2ad3bf99
Kinga Andrea Gemes <kinga.andrea.gemes@gmail.com>
2017-04-23 16:55:37 +0200

    Environment added

---
+++ frontyard/FrontYard.java
@@ -0,0 +1,33 @@
+// Environment code for project frontyard.mas2j
+
+import jason.asSyntax.*;
+import jason.environment.*;
+import java.util.logging.*;
+
+public class FrontYard extends Environment {
+
+    private Logger logger = Logger.getLogger("frontyard.mas2j."+FrontYard.class.getName());
+
+    /** Called before the MAS execution with the args informed in .mas2j */
+    @Override
+    public void init(String[] args) {
+        super.init(args);
+        addPercept(ASSyntax.parseLiteral("percept(demo)"));
+    }
+
+    @Override
+    public boolean executeAction(String agName, Structure action) {
+        logger.info("executing: "+action+", but not implemented!");
+        if (true) { // you may improve this condition
+             informAgsEnvironmentChanged();
+        }
+        return true; // the action was executed with success 
+    }
+
+    /** Called before the end of MAS execution */
+    @Override
+    public void stop() {
+        super.stop();
+    }
+}
+
